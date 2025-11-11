1d034acadff38e0625abf24030a5854d45c3a5e9
Deepthi <ddeepthidevara@gmail.com>
2025-11-11 21:01:20 +0530

    basic

---
+++ basic.java
@@ -0,0 +1,16 @@
+class basic{
+	public static void main(String [] args){
+	int x = 10;
+	double y = 100.200;
+	char z = 'a';
+	String w = "deepthi";
+	boolean i = true;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	System.out.println(w);
+	System.out.println(i);
+
+
+	}
+}
