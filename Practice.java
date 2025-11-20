d8aa9c2928dee512a884995ab583f7f1b2516a8f
Deepthi <ddeepthidevara@gmail.com>
2025-11-20 11:15:20 +0530

    new file

---
+++ Practice.java
@@ -0,0 +1,21 @@
+class Practice{
+	public static void main(String[] args){
+	int x = 1;
+	int [] array1 = {1,2,3,4,5};
+	System.out.println(x);
+	System.out.println(array1[2]);
+	System.out.println(array1.length);
+	float y = 2.5f;
+	float [] array2 = {1.2f,2.5f};
+	System.out.println(y);
+	int a = 20;
+	int b = 30;
+	System.out.println(a-b);
+	System.out.println(a<b);
+	
+
+	
+	
+	}
+
+}
